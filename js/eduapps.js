ed77a116ca9382a9746b662d4dd4d7e71d2c9da2
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2020-04-12 23:48:59 -0300

    test

--- js/eduapps.js
+++ js/eduapps.js
@@ -1,8 +1,32 @@
-var EduApps = {
+//var EduApps = {
 	
-	LoginFB: function() {
+	/*LoginFB: function() {
 		FB.getLoginStatus(function(__response) {
 			statusChangeCallback(__response);
 		});
+	},*/
+	
+function curl(url = "", ready) {
+	  var xhr = new XMLHttpRequest();
+	  //xhr.responseType = "application/text";
+	  
+	  xhr.open("GET", url);
+	  xhr.setRequestHeader('Content-Type', 'application/xml');
+	  xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+	  xhr.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  xhr.withCredentials = true;
+	  if (typeof ready === "function") {
+	    ready.apply(xhr);
+	  }
+	  
+	  
+	  xhr.onreadystatechange = function() {
+	    if (xhr.readyState === 4 && xhr.status === 200) {
+	      ready();
+	    } else if (xhr.readyState === 4 && xhr.status !== 200) {
+	      ready();
+	    }
+	  }
+	  xhr.send();
 	}
-};
+//};
